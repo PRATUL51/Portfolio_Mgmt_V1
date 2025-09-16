40a0cdd2edb697ee774fccb24bef1cd6fc42afa3
Pratul Dhara <PRATUL51@users.noreply.github.com>
2025-09-16 19:34:09 +0530

    Code Coverage - feature/customers 1. Unit Testing - Controller 2. UnitTesting - Service

--- src/main/java/com/java/portfolio/request/CustomerRequest.java
+++ src/main/java/com/java/portfolio/request/CustomerRequest.java
@@ -2,12 +2,14 @@ package com.java.portfolio.request;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 
 @Data
 @AllArgsConstructor
+@NoArgsConstructor
 public class CustomerRequest {
     private String panId;
     private String firstName;
--- src/main/java/com/java/portfolio/response/CustomerResponse.java
+++ src/main/java/com/java/portfolio/response/CustomerResponse.java
@@ -3,11 +3,13 @@ package com.java.portfolio.response;
 import com.java.portfolio.model.Customer;
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.util.List;
 
 @Data
 @AllArgsConstructor
+@NoArgsConstructor
 public class CustomerResponse {
     int code;
     String status;
